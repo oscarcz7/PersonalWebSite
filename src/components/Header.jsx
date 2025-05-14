9e2e17031534051045bfa8764b4cec8fe73ce22c
Oscar Catota <ocatotazurita@gmail.com>
2025-05-13 20:03:02 -0500

    Fix no display in header mobile and footer link about

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -122,10 +122,10 @@ function MobileNavigation(props) {
             </div>
             <nav className="mt-6">
               <ul className="-my-2 text-base divide-y divide-zinc-100 text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
-                {/* <MobileNavItem href="/about">About</MobileNavItem> */}
+                <MobileNavItem href="/about">About</MobileNavItem>
                 <MobileNavItem href="/articles">Articles</MobileNavItem>
                 {/* <MobileNavItem href="/projects">Projects</MobileNavItem> */}
-                <MobileNavItem href="/uses">Uses</MobileNavItem>
+                <MobileNavItem href="/uses">Set Up</MobileNavItem>
               </ul>
             </nav>
           </Popover.Panel>
@@ -165,7 +165,7 @@ function DesktopNavigation(props) {
         <NavItem href="/about">About </NavItem>
         <NavItem href="/articles">Articles</NavItem>
         {/* <NavItem href="/projects">Projects</NavItem> */}
-        <NavItem href="/uses">Uses</NavItem>
+        <NavItem href="/uses">Set Up</NavItem>
       </ul>
     </nav>
   )
