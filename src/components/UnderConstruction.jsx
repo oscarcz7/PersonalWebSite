6ca02078cdd2ee9ff7cf559ffaf4f855bf6c1688
Oscar Catota <ocatotazurita@gmail.com>
2025-05-14 21:10:41 -0500

    Add component for time build for projects page

---
+++ src/components/UnderConstruction.jsx
@@ -0,0 +1,48 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+import { Button } from '@/components/Button'
+
+const UnderConstruction = () => {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return null
+
+  return (
+    <div className="flex items-center justify-center min-h-screen ">
+      <div className="max-w-xl p-10 text-center">
+        <div className='mb-3'>
+          <svg
+            className="w-16 h-16 mx-auto text-teal-700"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 9v2m0 4h.01M4.93 4.93l14.14 14.14M4.93 19.07L19.07 4.93M12 2a10 10 0 100 20 10 10 0 000-20z"
+            />
+          </svg>
+          <h1 className="my-2 text-2xl font-bold text-gray-800">
+            Page Under Construction
+          </h1>
+          <p className="text-gray-600">
+            We’re working hard to improve your experience. Please check back
+            soon!
+          </p>
+        </div>
+
+        <Button type="submit" className="flex-none ml-4">
+          Return to Home
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default UnderConstruction
